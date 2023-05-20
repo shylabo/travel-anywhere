fe33552fb6dbfa708a9cabe22f12e4b520299a88
shylabo <shuya.sohot@gmail.com>
2023-05-20 12:25:51 -0700

    feat: implement login/signup ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import { Nunito } from 'next/font/google'
 import Navbar from './components/navbar/Navbar'
+import RegisterModal from './components/modals/RegisterModal'
+import ToasterProvider from './providers/ToasterProvider'
 
 export const metadata = {
   title: 'travel anywhere',
@@ -15,6 +17,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
+        <RegisterModal />
         <Navbar />
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return <div className="text-rose-500 text-2xl">Hello</div>
+  return <div className="text-sky-500 text-2xl">Hello</div>
 }
